5cb29998334a4ebdc2a066c80799b38a0e34882d
Biswajit Jana <biswajitworkmail@gmail.com>
2022-07-26 17:35:37 +0530

    transaction sample added on sample dal

--- src/data/sample.dal.ts
+++ src/data/sample.dal.ts
@@ -15,14 +15,17 @@ export default class SampleDal extends DBMSSQLBase {
     async getMunicipalitiesAsync(){
        
         try{
+            await this.beginTxnAsync();
             let cmd=new SqlCommand("taxation.Get_LocationByLocIdWardIdMnId")
             .addParam(new SqlParam("LocationID",1,sql.Int))
             .addParam(new SqlParam("WardId",1,sql.Int))
             .addParam(new SqlParam("MnId",47,sql.Int));
     
             let result =await this.executeAsync(cmd);
+           await this.commitTxnAsync()
             return result.recordset;
         }catch(e){
+            await this.rollbackTxnAsync();
             console.error(e)
         }
     }
